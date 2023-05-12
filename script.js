0169deec26395757bc63cca3b0878f60c4f6147d
HDelgadoM <125766164+HDelgadoM@users.noreply.github.com>
2023-05-12 18:30:36 -0500

    Carpeta principal

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function procesarRespuestas() {
-let total= 5
-let puntos=0;
+    let total = 5
+    let puntos = 0;
 
-let myForm = document.forms["formulario"];
-let respuetasCorrectas = ["c", "a", "b", "b", "a"];
+    let myForm = document.forms["formulario"];
+    let respuestasCorrectas = ["c", "a", "b", "b", "a"];
 
-for (let i=1; i <= total; i++) {
-    if (myForm["r" + i].value == null || 
-        myForm["r" + i].value == '') {
-        alert('Favor responder todas las preguntas');
-        return false;
+    for (let i = 1; i <= total; i++) {
+        if (myForm["r" + i].value == null ||
+            myForm["r" + i].value == '') {
+            alert('Favor responder todas las preguntas');
+            return false;
         } else {
-            if (myForm["r" + i].value === respuetasCorrectas[i - 1])
-            puntos++;
+            if (myForm["r" + i].value === respuestasCorrectas[i - 1])
+                puntos++;
         }
-}
-let resultado = document.getElementById('resultado')
-resultado.innerHTML='Obtuviste '+puntos+' puntos de ' + total + ' posibles ';
-return false;
+    }
+    let resultado = document.getElementById('resultado')
+    resultado.innerHTML = 'Obtuviste ' + puntos + ' puntos de ' + total + ' posibles ';
+    return false;
 }
